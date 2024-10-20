dfc2df001f3d6087689483387df610e11d13ef3b
Rowan Chander <chander89@live.nl>
2024-10-20 18:51:34 +0200

    Test: Started on the BookService tests

--- MediaCollection.Business.Testing/ServiceTests/BookServiceTests/GetTests.cs
+++ MediaCollection.Business.Testing/ServiceTests/BookServiceTests/GetTests.cs
@@ -5,6 +5,51 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MediaCollection.Business.Testing.ServiceTests.BookServiceTests;
-internal class GetTests
+
+[ExcludeFromCodeCoverage]
+public class GetTests
 {
+    private readonly Book[] _mockedBooks =
+    [
+        new Book { Id = 1, Name = "Huckleberry Finn" },
+        new Book { Id = 2, Name = "Alice in Wonderland" },
+        new Book { Id = 3, Name = "Kill all Normies" },
+    ];
+
+    [Fact]
+    public async void SHould_ReturnCorrectAmountOfBooks()
+    {
+        // -- Arrange
+        Mock<IBookRepository> mockedBookRepository = new Mock<IBookRepository>();
+        IBookService bookService = new BookService(mockedBookRepository.Object);
+        mockedBookRepository.Setup(bRepo => bRepo.Get()).ReturnsAsync(_mockedBooks.AsQueryable());
+        int pageNumber = 1;
+        int pageSize = 10;
+
+        // -- Act
+        var (books, metadata) = await bookService.Get(pageNumber, pageSize);
+
+        // -- Assert
+        books.ToList().Should().HaveCount(3);
+    }
+
+    [Fact]
+    public async Task Should_HaveCorrectPaginationMetaData()
+    {
+        // -- Arrange
+        Mock<IBookRepository> mockedBookRepository = new Mock<IBookRepository>();
+        IBookService bookService = new BookService(mockedBookRepository.Object);
+        mockedBookRepository.Setup(bRepo => bRepo.Get()).ReturnsAsync(_mockedBooks.AsQueryable());
+        int pageNumber = 1;
+        int pageSize = 2;
+
+        // -- Act
+        var (books, metadata) = await bookService.Get(pageNumber, pageSize);
+
+        // -- Assert
+        metadata.PageSize.Should().Be(2);
+        metadata.CurrentPage.Should().Be(1);
+        metadata.TotalItemCount.Should().Be(3);
+        metadata.TotalPageCount.Should().Be(2);
+    }
 }
