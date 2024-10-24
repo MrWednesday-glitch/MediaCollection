fa6ebbc3b46b7bae47e9063a1a85d93d0cbac41d
Rowan Chander <chander89@live.nl>
2024-10-24 19:58:50 +0200

    Test: Wrote the constructorTests.

---
+++ MediaCollection.Business.Testing/ServiceTests/BookServiceTests/ConstructorTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MediaCollection.Business.Testing.ServiceTests.BookServiceTests;
+
+public class ConstructorTests
+{
+    [Fact]
+    public void SHould_ThrowAnArgumentNullException_When_BookRepositoryIsNull()
+    {
+        // -- Arrange
+
+        // -- Act
+        Action testIncovation = () => _ = new BookService(null!);
+
+        // -- Assert
+        testIncovation
+            .Should().ThrowExactly<ArgumentNullException>()
+            .WithParameterName("bookRepository");
+    }
+}
---
+++ MediaCollection.Business.Testing/ServiceTests/FilmServiceTests/ConstructorTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MediaCollection.Business.Testing.ServiceTests.FilmServiceTests;
+
+public class ConstructorTests
+{
+    [Fact]
+    public void Should_ThrowAnArgumentNullException_When_FilmRepositoryIsNull()
+    {
+        // -- Arrange
+
+        // -- Act
+        Action testIncovation = () => _ = new FilmService(null!);
+
+        // -- Assert
+        testIncovation
+            .Should().ThrowExactly<ArgumentNullException>()
+            .WithParameterName("filmRepository");
+    }
+}
