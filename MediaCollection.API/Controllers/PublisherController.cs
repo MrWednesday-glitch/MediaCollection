c7f05ba80e4edfbba65ea2b35f5cfabd22b72bcb
Rowan <Rowan@DESKTOP-BSS8KAA>
2025-12-31 14:47:25 +0100

    Feat: Setup the base controller

---
+++ MediaCollection.API/Controllers/PublisherController.cs
@@ -0,0 +1,35 @@
+﻿using MediaCollection.Domain.Models;
+
+namespace MediaCollection.API.Controllers;
+
+// TODO Summaries
+//TODO Unit tests
+[ApiController]
+[Route("publishers")]
+public class PublisherController : ControllerBase
+{
+    // TODO Make service, interface, and add to DI container
+    private readonly IPublisherService _publisherService;
+
+    public PublisherController(IPublisherService publisherService)
+    {
+        ArgumentNullException.ThrowIfNull(publisherService);
+
+        _publisherService = publisherService;
+    }
+
+    [HttpPost(Name = "PostPublishers")]
+    public async Task<IActionResult> PostPublishers([FromBody]PublisherToBe[] publisherToBe)
+    {
+        try
+        {
+            Publisher[] createdPublishers = ...;
+
+            return Created("", createdPublishers);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
