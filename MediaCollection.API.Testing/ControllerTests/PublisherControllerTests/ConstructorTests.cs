d4265efaf5bc2699ca1ae76226efb1f317484c05
Rowan <Rowan@DESKTOP-BSS8KAA>
2026-01-02 10:12:46 +0100

    Test: Set up the Api testproject

---
+++ MediaCollection.API.Testing/ControllerTests/PublisherControllerTests/ConstructorTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MediaCollection.API.Testing.ControllerTests.PublisherControllerTests;
+
+public class ConstructorTests
+{
+    [Fact]
+    public void Should_ThrowAnArgumentNullException_When_PublisherServiceIsNull()
+    {
+        // -- Arrange
+
+        // -- Act
+        Action testIncovation = () => _ = new PublisherController(null!);
+
+        // -- Assert
+        testIncovation
+            .Should().ThrowExactly<ArgumentNullException>()
+            .WithParameterName("publisherService");
+    }
+}
