f001c9621038ac3a3b4d60739a06870da6ebeba3
Rowan <Rowan@DESKTOP-BSS8KAA>
2026-01-02 11:27:51 +0100

    Chore: Added nuget packages, moved some globals to the .csproj, and added some usings.

--- MediaCollection.API.Testing/GlobalUsings.cs
+++ MediaCollection.API.Testing/GlobalUsings.cs
@@ -1,2 +1,7 @@
-﻿global using FluentAssertions;
-global using MediaCollection.API.Controllers;
+﻿global using MediaCollection.API.Controllers;
+global using MediaCollection.Domain.Entities;
+global using MediaCollection.Domain.Interfaces;
+global using MediaCollection.Domain.Models;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Diagnostics.CodeAnalysis;
