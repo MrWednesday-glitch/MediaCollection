2245a60e00cce33f38e5edfd82d0bcbf1165ff2d
Rowan <Rowan@DESKTOP-BSS8KAA>
2026-01-09 16:46:38 +0100

    Feat: Wrote the IdeveloperRepo

---
+++ MediaCollection.Domain/Interfaces/IDeveloperRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaCollection.Domain.Interfaces;
+
+// TODO SUmmaries
+public interface IDeveloperRepository : IRepository<Developer>
+{
+}
