d60677b0c8bbd6f8d1dda215b6bd51fbdb5c53b2
Rowan <Rowan@DESKTOP-BSS8KAA>
2026-01-09 16:17:47 +0100

    Feat: Made the developerToBe record

---
+++ MediaCollection.Domain/Models/DeveloperToBe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaCollection.Domain.Models;
+
+// TODO Summaries
+public record DeveloperToBe
+{
+    public string? PictureUri { get; init; }
+
+    public string Name { get; init; } = string.Empty;
+}
