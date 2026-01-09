1b1026c30212aa14be5373a1c4f0f1d96f025064
Rowan <Rowan@DESKTOP-BSS8KAA>
2026-01-09 16:29:28 +0100

    Feat: Wrote the IDeveloperService

---
+++ MediaCollection.Domain/Interfaces/IDeveloperService.cs
@@ -0,0 +1,12 @@
+﻿using MediaCollection.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaCollection.Domain.Interfaces;
+
+// TODO Summaries
+public interface IDeveloperService
+{
+    Task<IEnumerable<Developer>> Add(IEnumerable<DeveloperToBe> developersToBe);
+}
