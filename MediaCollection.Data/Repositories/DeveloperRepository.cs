964545765b70b3cd71c4a948752df2d7e04d96d1
Rowan <Rowan@DESKTOP-BSS8KAA>
2026-01-09 16:46:57 +0100

    Feat: Wrote the developerrepo

---
+++ MediaCollection.Data/Repositories/DeveloperRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaCollection.Data.Repositories;
+
+// TODO Summaries
+[ExcludeFromCodeCoverage]
+public class DeveloperRepository : EFRepository<Developer>, IDeveloperRepository
+{
+    public DeveloperRepository(MediaDbContext mediaDbContext) : base(mediaDbContext)
+    {
+    }
+
+    public override async Task CreateRecord(Developer entity)
+    {
+        await base.CreateRecord(entity);
+    }
+
+    public override async Task CreateRecords(IEnumerable<Developer> entities)
+    {
+        await base.CreateRecords(entities);
+    }
+    public override async Task DeleteRecord(Developer entity)
+    {
+        await base.DeleteRecord(entity);
+    }
+
+    public override async Task<IQueryable<Developer>> Get()
+    {
+        return await base.Get();
+    }
+
+    public override async Task<Developer> Get(Guid id)
+    {
+        return await base.Get(id) ?? throw new KeyNotFoundException($"No developer with id {id} was found.");
+    }
+
+    public override async Task SaveChanges()
+    {
+        await base.SaveChanges();
+    }
+}
