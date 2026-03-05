2826bbb2d53b98631488504951d982c74df96781
Rowan <Rowan@DESKTOP-BSS8KAA>
2026-03-05 13:44:43 +0100

    Test: Made unit tests

---
+++ MediaCollection.Domain.Testing/CustomErrorTests/RecordNotFoundTests.cs
@@ -0,0 +1,21 @@
+﻿namespace MediaCollection.Domain.Testing.CustomErrorTests;
+
+[ExcludeFromCodeCoverage]
+public class RecordNotFoundTests
+{
+    [Fact]
+    public void Should_CreateErrorWithProvidedMessageAndStatusCode()
+    {
+        // -- Arrange
+        string message = "Record not found";
+        int statusCode = 404;
+
+        // -- Act
+        CustomError customError = CustomError.RecordNotFound(message, statusCode);
+
+        // -- Assert
+        customError.Code.Should().Be(ErrorCodes.RecordNotFound);
+        customError.Should().BeEquivalentTo(new CustomError(ErrorCodes.RecordNotFound,
+            new CustomErrorInformation(404, "Record not found")));
+    }
+}
