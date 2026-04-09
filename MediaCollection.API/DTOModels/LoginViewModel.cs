535c831a50735e77d560e41ec1c40aa884c5de2f
Rowan <Rowan@DESKTOP-BSS8KAA>
2026-04-09 11:42:49 +0200

    Feat: Made the account models

---
+++ MediaCollection.API/DTOModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MediaCollection.API.DTOModels;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Email Id is Required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password Id is Required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [Display(Name = "Remember Me")]
+    public bool RememberMe { get; set; }
+}
---
+++ MediaCollection.API/DTOModels/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaCollection.API.DTOModels;
+
+public class ProfileViewModel
+{
+    public string Email { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string? LastName { get; set; }
+    public DateTime? LastLoggedIn { get; set; }
+    public DateTime? CreatedOn { get; set; }
+}
